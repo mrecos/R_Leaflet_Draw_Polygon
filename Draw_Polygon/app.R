44209277277e62e7ed2811a00695e61c2edd211f
Matt Harris <mr.ecos@gmail.com>
2022-06-07 20:00:34 -0400

    update with s dataframe and download button

--- Draw_Polygon/app.R
+++ Draw_Polygon/app.R
@@ -15,7 +15,10 @@ ui <-
       title = "Draw Polygons App"
     ),
     dashboardSidebar(
-      
+      textInput("file_name_input", "enter file name",
+                placeholder = "Default"),
+      # Save Button
+      downloadButton("downloadData", "Download")
     ),
     dashboardBody(
       useShinyjs(),
@@ -26,10 +29,12 @@ ui <-
         column(width = 9,
                box(width = NULL, solidHeader = TRUE,
                    leafletOutput("map", height = 500)
-               ),
-               box(width = NULL,
-                   tableOutput("tbl")
                )
+        ),
+        column(width = 3,
+          box(width = NULL, solidHeader = TRUE, height = 500,
+              tableOutput("tbl")
+            )
         )
       )
     )
@@ -38,26 +43,44 @@ ui <-
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   
+  # reactive container for the drawn polygon shapes
+  polygons <- reactiveValues(
+    shapes = NULL
+  )
+  
+  # drawn map
   output$map <- renderLeaflet({
-    cat("output$map","\n")
     leaflet() %>%
       addProviderTiles(providers$CartoDB.Positron) %>%
       setView(lng = -98, lat = 39, zoom = 4) %>% 
       addDrawToolbar()
   })
   
-  mymap_proxy <- leafletProxy("map")
+  # proxy not in use at the moment
+  # mymap_proxy <- leafletProxy("map")
   
+  # Watch for new polygons to be drawn and add to sf dataframe
   observeEvent(input$map_draw_new_feature,{
     feature <- input$map_draw_new_feature
     
     feature_sf <- geojsonsf::geojson_sf(jsonify::to_json(feature, unbox = T))
-    print(feature_sf)
     
-    output$tbl <- renderTable(st_drop_geometry(feature_sf))
+    polygons$shapes <- rbind(polygons$shapes, feature_sf)
+    print(polygons$shapes)
+    
+    output$tbl <- renderTable(st_drop_geometry(polygons$shapes))
     
   })
   
+  # Downloadable geojson of polygons ----
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste(input$file_name_input, ".geojson", sep = "")
+    },
+    content = function(file) {
+      write_sf(polygons$shapes, file)
+    }
+  )
   
 }
 

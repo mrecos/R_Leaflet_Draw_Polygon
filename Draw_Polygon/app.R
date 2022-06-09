e8f7ab46cbbedb591fd90ebc47742c79b6d0f06d
Matt Harris <mr.ecos@gmail.com>
2022-06-09 16:55:10 -0400

    only square and polygon inputs. basic table output. starting new branch for DT output and editing

--- Draw_Polygon/app.R
+++ Draw_Polygon/app.R
@@ -7,6 +7,8 @@ library(shinyWidgets)
 library(dplyr)
 library(sf)
 library(leaflet.extras)
+library(DT)
+library(rhandsontable)
 
 # Define UI for application that draws a histogram
 ui <- 
@@ -38,15 +40,18 @@ ui <-
         theme = "grey_light"
       ),
       fluidRow(
-        column(width = 9,
+        column(width = 12,
                box(width = NULL, solidHeader = TRUE,
                    leafletOutput("map", height = 500)
                )
-        ),
-        column(width = 3,
-          box(width = NULL, solidHeader = TRUE, height = 500,
-              tableOutput("tbl")
-            )
+        )
+      ),
+      fluidRow(
+        column(width = 12,
+               box(width = NULL, solidHeader = TRUE, height = 500,
+                   tableOutput("tbl")
+                   # DTOutput("table1")
+               )
         )
       )
     )
@@ -59,6 +64,20 @@ server <- function(input, output) {
     shapes = NULL
   )
   
+  # reactive container for table values
+  # kept separate from coords b/c DT does not to sf objects
+  # part of the design to allow edits/updates to table
+  data_table <- reactiveValues(
+    # DTtable = NULL
+    DTtable = NULL
+  )
+  
+  # output$table1 <- renderDT({
+  #   req(data_table$DTtable)
+  #   cols <- setdiff(colnames(data_table$DTtable),"_leaflet_id")
+  #   datatable(data_table$DTtable[,cols], editable = TRUE)
+  # })
+  
   # drawn map
   output$map <- renderLeaflet({
     leaflet() %>%
@@ -69,7 +88,11 @@ server <- function(input, output) {
         baseGroups = c("CartoDB.Positron", "OpenStreetMap","Esri.WorldImagery"),
         position = "topright") %>% 
       setView(lng = -98, lat = 39, zoom = 4) %>% 
-      addDrawToolbar()
+      addDrawToolbar(circleOptions = FALSE,
+                     
+                     polylineOptions = FALSE,
+                     markerOptions = FALSE,
+                     circleMarkerOptions = FALSE)
   })
   
   # proxy not in use at the moment
@@ -78,13 +101,17 @@ server <- function(input, output) {
   # Watch for new polygons to be drawn and add to sf dataframe
   observeEvent(input$map_draw_new_feature,{
     feature <- input$map_draw_new_feature
-  print(input$filetype)
     feature_sf <- geojsonsf::geojson_sf(jsonify::to_json(feature, unbox = T))
     
     polygons$shapes <- rbind(polygons$shapes, feature_sf)
     print(polygons$shapes)
     
     output$tbl <- renderTable(st_drop_geometry(polygons$shapes))
+    # DT stuff
+    # step_table <- st_drop_geometry(polygons$shapes)
+    # step_table$LABEL <- NA
+    # data_table$DTtable <- rbind(data_table$DTtable,
+    #                             step_table)
     
   })
   
